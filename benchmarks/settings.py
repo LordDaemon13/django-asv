fa85e89cf13e43ad8d26e9e07fd23187ec57461e
Deepak Dinesh <d.deepakdinesh13@gmail.com>
2022-05-18 07:02:36 +0530

    url resolve flat i8n off benchmark added

--- benchmarks/settings.py
+++ benchmarks/settings.py
@@ -13,6 +13,7 @@ INSTALLED_APPS = [
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "benchmarks",
+    "benchmarks.url_benchmarks.url_resolve_flat_i18n_off"
 ]
 
 SECRET_KEY = "NOT REALLY SECRET"
