27ce12787b37e425debc39a238a91439b6cc5cf4
Deepak Dinesh <d.deepakdinesh13@gmail.com>
2022-06-04 09:51:11 +0530

    benchmarks in bench_urls.py refactored and raw_sql benchmark fixed

--- benchmarks/settings.py
+++ benchmarks/settings.py
@@ -24,6 +24,10 @@ INSTALLED_APPS = [
     "benchmarks.model_benchmarks.model_save_new",
     "benchmarks.model_benchmarks.model_save_existing",
     "benchmarks.other_benchmarks.raw_sql",
+    "benchmarks.url_benchmarks.url_resolve",
+    "benchmarks.url_benchmarks.url_resolve_flat",
+    "benchmarks.url_benchmarks.url_resolve_nested",
+    "benchmarks.url_benchmarks.url_reverse",
 ]
 
 SECRET_KEY = "NOT REALLY SECRET"
